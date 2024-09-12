f433b1e5fa9722a92f1ee3e3feed5272cce8209a
Spit <153082128+spitmirror@users.noreply.github.com>
2024-09-12 21:02:44 +0100

    Update yt-playlist-sync.sh

--- yt-playlist-sync.sh
+++ yt-playlist-sync.sh
@@ -13,31 +13,30 @@ YT_URL="https://www.youtube.com"
 WATCH_URL="$YT_URL/watch?v="
 
 # Create an array of videos IDs from online playlist
+printf  "Downloading all watch IDs..."; sleep 1; echo " (this might take a while)"
 readarray -t ONLINE < <(yt-dlp --skip-download --print id $PLAYLIST_URL 2> /dev/null)
-declare -p ONLINE 
-# echo ONLINE IS ${ONLINE[@]}
+declare ONLINE
 
 # Create an array of video IDs from offline playlist
 readarray -t OFFLINE < <(ls -1 $PLAYLIST_DIR/*.mp4 | sed -E 's/.*\[([^]]+)\].*/\1/')
-declare -p OFFLINE 
-# echo OFFLINE IS ${OFFLINE[@]}
+declare OFFLINE
 
 # Remove any videos no longer in online playlist
 for id in "${OFFLINE[@]}"; do
-  echo ${ONLINE[@]} | grep -q $id
+  echo "${ONLINE[@]}" | grep -q -- $id
   if [ "$?" -ne 0 ]; then
     tmp=$(find $PLAYLIST_DIR -name \*$id\*)
-    echo "Deleting $(basename "$tmp")..."
-    rm "$tmp"
+    echo "-$(basename "$tmp")..."
+    # rm "$tmp"
   fi
 done
 echo
 
 # Download any new videos from online playlist
 for id in "${ONLINE[@]}"; do
-  echo ${OFFLINE[@]} | grep -q $id 
+  echo ${OFFLINE[@]} | grep -q -- $id
   if [ "$?" -ne 0 ]; then
-    echo "Downloading ${WATCH_URL}${id}"
-    yt-dlp -q -f mp4 -P $PLAYLIST_DIR --playlist-random ${WATCH_URL}${id}
+    echo "+${WATCH_URL}${id}"
+    # yt-dlp -q -f mp4 -P $PLAYLIST_DIR --playlist-random ${WATCH_URL}${id}
   fi
 done
